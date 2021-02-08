fe5cac99a68288489b6f2534a7aaea2b64f2fe22
Igor Pansini <pansini@outlook.com.br>
2021-02-08 19:44:16 -0300

    Rotas e nodemon

--- Express/index.js
+++ Express/index.js
@@ -1,10 +1,20 @@
 const express = require("express"); //Importando o express.
 const app = express(); //Iniciando o express.
 
+//Criando rotas.
+app.get("/", function (req, res) {
+    res.send("Home Page.");
+});
+
+app.get("/test", function (req, res) {
+    res.send("Test.");
+})
+
+//Iniciando o servidor na porta 8181. 
 app.listen(8181, function (erro) {
     if (erro) {
-        console.log("Erro!");
+        console.log("Error.");
     } else {
-        console.log("Sucesso!")
+        console.log("Success.");
     }
-});
+})
