dd27b22077c2fb742b4e0d9ed6c87c8624ffb29e
Igor Pansini <pansini@outlook.com.br>
2021-02-09 12:48:52 -0300

    Arquivos estaticos e instalando o Bootstrap.

--- AskMe-Projeto01/index.js
+++ AskMe-Projeto01/index.js
@@ -4,6 +4,8 @@ const app = express();
 //Usando EJS como view engine. 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
