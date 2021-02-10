23d062fb86dc70d354ee962450629a84b294301f
Igor Pansini <pansini@outlook.com.br>
2021-02-10 11:57:27 -0300

    Body Parser e Sequelize

--- AskMe-Projeto01/index.js
+++ AskMe-Projeto01/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Usando EJS como view engine. 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -15,6 +19,8 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/perguntafeita", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
     res.send("Formulario Enviado.");
 });
 
