a41e5c735b8243260927edf3dc9053fc0463796a
Igor Pansini <pansini@outlook.com.br>
2021-02-17 11:07:10 -0300

    Criando formulario de artigo e TinyMCE

--- SchumiPress-Proj02/articles/ArticlesController.js
+++ SchumiPress-Proj02/articles/ArticlesController.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/articles", (req, res) => {
-    res.send("Rota dos artigos.");
+router.get("/admin/articles/new", (req, res) => {
+    res.render("admin/articles/new");
 });
 
 module.exports = router;
