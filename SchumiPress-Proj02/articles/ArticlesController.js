11a0120f0db76df7f8afbe786d4a2f047d3358dc
Igor Pansini <pansini@outlook.com.br>
2021-03-15 22:27:45 -0300

    Iniciando paginacao

--- SchumiPress-Proj02/articles/ArticlesController.js
+++ SchumiPress-Proj02/articles/ArticlesController.js
@@ -86,4 +86,37 @@ router.post("/articles/update", (req, res) => {
     })
 });
 
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset;
+
+    if (isNaN(page) || page == 1) {
+        offset = 0;
+    } else {
+        offset = (parseInt(page) - 1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+        var next;
+
+        if (offset + 4 >= articles.count) {
+            next = false;
+        } else {
+            next = true;
+        }
+
+        var result = {
+            articles: articles,
+            next: next
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", { result: result, categories: categories });
+        });
+    })
+});
+
 module.exports = router;
